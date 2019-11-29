e37504030a11ca9f2a7a53e6ba26ba09f1a620c5
Drumsid <den-sidnay@yandex.ru>
2019-11-29 23:36:00 +0300

    solution first step?

--- bin/testing.php
+++ bin/testing.php
@@ -1,26 +1,25 @@
 <?php
 
-$autoloadPath1 = __DIR__ . '/../../../autoload.php';
-$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
-if (file_exists($autoloadPath1)) {
-    require_once $autoloadPath1;
-} else {
-    require_once $autoloadPath2;
-}
+// $autoloadPath1 = __DIR__ . '/../../../autoload.php';
+// $autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
+// if (file_exists($autoloadPath1)) {
+//     require_once $autoloadPath1;
+// } else {
+//     require_once $autoloadPath2;
+// }
+//
+// $gendiff = <<<DOC
+// Naval Fate.
+//
+// Usage:
+//   naval_fate.php (-h | --help)
+//   naval_fate.php --version
+//
+// Options:
+//   -h --help     Show this screen.
+//   --version     Show version.
+//
+// DOC;
 
-$gendiff = <<<DOC
-Naval Fate.
-
-Usage:
-  naval_fate.php ship new gendiff
-  naval_fate.php (-h | --help)
-  naval_fate.php --version
-
-Options:
-  -h --help     Show this screen.
-  --version     Show version.
-
-DOC;
-
-$args = Docopt::handle($gendiff);
-print_r($args);
+// $args = Docopt::handle($gendiff);
+// print_r($args);
